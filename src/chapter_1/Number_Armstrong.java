1a0e2f43d59a5847e89856626431639d38bbbb57
Yzabakli <abakliyusufziya@gmail.com>
2022-08-08 21:33:48 +0200

    Number_Armstrong

--- src/chapter_1/Number_Armstrong.java
+++ src/chapter_1/Number_Armstrong.java
@@ -6,8 +6,12 @@ public class Number_Armstrong {
     Write a method that can check if a number is Armstrong number
      */
 
+    public static void main(String[] args) {
+
+        System.out.println(isArmStrongNumber(132));
+    }
     public static boolean isArmStrongNumber(int num) {
-        int a = 0, b = 0, c = num;
+        int a, b = 0, c = num;
 
         while (num > 0) {
             a = num % 10;
@@ -15,7 +19,7 @@ public class Number_Armstrong {
             b = b + (a * a * a);
         }
 
-        return (c == b) ? true : false;
+        return c == b;
     }
 
 }
